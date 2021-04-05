67477f8a0ca538787e1766866ee93907952a1d98
xiexingen <1002275364@qq.com>
2021-04-05 10:34:52 +0800

    案例代码

---
+++ samples/parseParam/index.js
@@ -0,0 +1,62 @@
+/**
+ * 解析url中的query转换成对象
+ * @param {string} url 要解析的url
+ * @returns Object
+ */
+function parseQueryString(url) {
+  // 提取url中？后面的字符串
+  const queryStr=/.+\?(.+)$/.exec(url)[1]
+  const queryArr=queryStr.split('&');
+  const paramsObj={}
+  queryArr.forEach(param=>{
+    if(/=/.test(param)){
+      // 使用= 分隔键和值
+      let [key,val]=param.split('=');
+      // 解码
+      val=decodeURIComponent(val);
+      // 判断是否数字，并转换
+      val=/^\d+$/.test(val) ? parseFloat(val) : val
+      // 如果有重复的key，则转换为数组
+      if(paramsObj.hasOwnProperty(key)){
+        paramsObj[key]=[].concat(paramsObj[key], val)
+      }else{
+        paramsObj[key]=val;
+      }
+    }
+    // 没有=赋值的算作true
+    else{
+      paramsObj[param] = true;
+    }
+  })
+
+  return paramsObj;
+}
+
+function stringifyObj(obj){
+  const result=[]
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      const value = obj[key];
+      result.push(`${key}=${encodeURIComponent(value)}`);
+    }
+  }
+  return result.join('&');
+}
+
+(function init() {
+  const txtUrl = document.getElementById("url");
+  const txtStringify=document.getElementById("stringify");
+
+  // 将输入的url解析成对象
+  document.getElementById("btnParse").onclick = function () {
+    const url = txtUrl.value;
+    const result = parseQueryString(url);
+    txtStringify.value = JSON.stringify(result);
+  };
+
+  document.getElementById('btnStringify').onclick=function(){
+    const obj = JSON.parse(txtStringify.value);
+    const query = stringifyObj(obj);
+    txtUrl.value = query;
+  }
+})();
---
+++ samples/throttle/index.js
@@ -0,0 +1,69 @@
+/**
+ * 函数节流
+ * - 支持立即执行
+ * - 支持取消功能
+ * - 函数可能有返回值
+ * @param {function} func 执行的事件
+ * @param {number} wait 高频延迟时间 毫秒
+ * @param options.trailing // 是否可以立即执行一次
+ * @param options.leading // 结束调用的时候是否还要执行一次
+ * @returns function
+ */
+function throttle(func, wait, options) {
+  let timeout, context, args;
+  let previous = 0;
+  if (!options) {
+    options = {};
+  }
+
+  const later = function () {
+    previous = options.leading === false ? 0 : new Date().getTime();
+    timeout = null;
+    func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+
+  const throttled = function () {
+    const now = new Date().getTime();
+    if (!previous && options.leading === false) previous = now;
+    const remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+  };
+
+  throttled.cancel = function () {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = null;
+  };
+
+  return throttled;
+}
+
+(function init() {
+  // 定义节流,立即执行
+  const throttleGetUserAction = throttle(getUserAction, 1000);
+  // 使用节流绑定
+  document.body.onmousemove = throttleGetUserAction;
+
+  // 取消节流
+  // throttleGetUserAction.cancel();
+
+  // 输出鼠标坐标到界面上
+  function getUserAction(e) {
+    const p = document.createElement("p");
+    p.innerText = `clientX:${e.clientX},clientY:${e.clientX}`;
+    document.body.appendChild(p);
+  }
+})();
