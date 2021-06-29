a0fcd68ebb2d5e01f8e391f2698fd73146f5aa80
xiexingen <1002275364@qq.com>
2021-06-29 09:41:03 +0800

    add html parse

---
+++ samples/src/html-parse/index.js
@@ -0,0 +1,19 @@
+
+const domTree = htmlParser(`
+<body>
+    <div>
+        <!--button-->
+        <button style="font-size:12px">按钮</button>
+        <div id="container">
+            <div class="box1">
+              <p>box1 box1 box1</p>
+            </div>
+            <div class="box2">
+                <p>box2 box2 box2</p>
+            </div>
+        </div>
+    </div>
+</body>
+`);
+
+console.log(JSON.stringify(domTree, null, 4));
