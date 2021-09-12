162238ee0e380a5e5c1b95dd052ffb1c8af3419b
xiexingen <1002275364@qq.com>
2021-09-12 10:29:12 +0800

    add webpack

---
+++ webpack/src/constants.js
@@ -0,0 +1,3 @@
+export const PAGE_SIZE = 20;
+
+export const DEFAULT_LANGUAGE = "chinese";
---
+++ webpack/src/message.js
@@ -0,0 +1,5 @@
+import { DEFAULT_LANGUAGE } from "./constants";
+
+export default function sayHello() {
+  console.log(`hello,default language is:${DEFAULT_LANGUAGE}`);
+}
