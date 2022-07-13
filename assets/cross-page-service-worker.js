2cb825c3c76e9b69d9f452dfa757cb8cc4f9d43b
xiexingen <xiexingen@users.noreply.github.com>
2022-07-13 03:29:00 +0000

    deploy: 7697352ea98cd65cfc71cff82c5637402be7a4d9

--- assets/cross-page-service-worker.js
+++ assets/cross-page-service-worker.js
@@ -1,1 +1,34 @@
-this.addEventListener("install",(function(t){t.waitUntil(caches.open("test").then((function(t){return t.addAll(["./note/skill/cross-page"])})))})),this.addEventListener("fetch",(function(t){t.respondWith(caches.match(t.request))})),this.addEventListener("message",(function(t){this.clients.matchAll().then((function(e){e.forEach((function(e){e.focused||e.postMessage(t.data)}))}))}));
+this.addEventListener('install', function (event) {
+  // 通过这个方法可以防止缓存未完成就关闭serviceWorker
+  event.waitUntil(
+    // 创建一个名叫test的缓存版本
+    caches.open('test').then(function (cache) {
+      // 指定要缓存的内容，地址为相对于跟域名的访问路径
+      return cache.addAll(['./note/skill/cross-page']);
+    }),
+  );
+});
+
+/**
+ * 注册fetch事件，拦截全站的请求
+ */
+this.addEventListener('fetch', function (event) {
+  event.respondWith(
+    // 在缓存中匹配对应请求资源直接返回
+    caches.match(event.request),
+  );
+});
+
+/**
+ * 监听消息，通知其他 Tab 页面
+ */
+this.addEventListener('message', function (event) {
+  this.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      // 这里的判断目的是过滤掉当前 Tab 页面，也可以使用 visibilityState 的状态来判断
+      if (!client.focused) {
+        client.postMessage(event.data);
+      }
+    });
+  });
+});
